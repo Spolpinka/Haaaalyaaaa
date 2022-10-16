fd8fed0cd351205d1a00fbfe65e123ac5d5e5f41
Spolpinka <a421243266@gmail.com>
2022-10-16 15:32:12 +0300

    Галины позывные

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
 public class Main {
+    private static Queue<String> queue1 = new LinkedList<>();
+    private static Queue<String> queue2 = new LinkedList<>();
+    private static final int MAX_NUMB = 5;
     public static void main(String[] args) {
-        System.out.println();
+        for (int i = 0; i < 12; i++) {
+            addPerson("Person " + i);
+        }
+        System.out.println("Итого:\n" +
+                "длина первой очереди " + queue1.size() + "\n" +
+                "длина второй очереди " + queue2.size());
+
+    }
+
+    private static void addPerson(String s) {
+        Random r = new Random();
+        if (queue2.size() > queue1.size() && queue1.size() < MAX_NUMB) {
+            queue1.offer(s);
+            System.out.println("в очередь 1 добавлен человечек " + s);
+        } else if (queue2.size() < MAX_NUMB){
+            queue2.offer(s);
+            System.out.println("в очередь 2 добавлен человечек " + s);
+        } else {
+            System.out.println("Хааааляааааа!!! Траблы!!!");
+        }
     }
 }
